48eb68bd62e4ee672358299ad806d54331ab47b2
vallenov <a2lenov@mail.ru>
2020-08-31 00:11:02 +0300

    Add random size of generating file

--- generator.py
+++ generator.py
@@ -6,7 +6,7 @@ import shutil
 def genfiles(dir):
     i = 0
     lst = os.listdir(dir)
-    while i < 100:
+    while True: #i < 100
         way = random.randint(0,len(lst)-1)
         tm = time.gmtime(time.time())
         if not os.path.exists(dir+'\\'+lst[way]+'\\'+str(tm.tm_year)):
@@ -18,11 +18,11 @@ def genfiles(dir):
         filename = 'file'+str(tm.tm_year)+'-'+str(tm.tm_mon)+'-'+str(tm.tm_mday)+'_'+str(tm.tm_hour)+str(tm.tm_min)+str(tm.tm_sec)
         f = open(dir+'\\'+lst[way]+'\\'+str(tm.tm_year)+'\\'+str(tm.tm_mon)+'\\'+str(tm.tm_mday)+'\\'+str(filename), 'w')
         print(str(filename)+' create')
-        for inf in range(1000000):
+        for inf in range(random.randint(500000,1000000)):
             f.write('test ')
         print(str(filename)+' fill')
         f.close
-        i += 1
+        #i += 1
         #time.sleep(random.randint(2,5))
 
 genfiles(r'C:\Prog\Python\NFSR\receive')
