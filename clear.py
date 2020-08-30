de329b54cddf2db64e0545b06095be26c1024cf2
vallenov <a2lenov@mail.ru>
2020-08-31 00:12:16 +0300

    Add logs cleaning

--- clear.py
+++ clear.py
@@ -6,6 +6,13 @@ def clear_way(dir):
     for directory in os.listdir(dir):
         for file in os.listdir(dir+'\\'+directory):
             shutil.rmtree(dir+'\\'+directory+'\\'+file)
+    try:
+        err = open('error.log', 'w')
+        move = open('move.log', 'w')
+    except FileNotFoundError:
+        print('File not found!')
+    else:
+        print('Clear log files done')
     print('Clear {} done'.format(dir))
 
 clear_way(r'C:\Prog\Python\NFSR\receive')
